7edf6f10374d237587f5d476140c2167c01c6ef1
rustambekovich <muhammadqodir5050@gmail.com>
2023-10-17 18:56:45 +0500

    docker-update

---
+++ src/RevisionVR.WebApi/Hubs/UserPositionHub.cs
@@ -0,0 +1,17 @@
+﻿using RevisionVR.WepApi.IHubs;
+using Microsoft.AspNetCore.SignalR;
+using RevisionVR.Service.DTOs.Positions;
+
+namespace RevisionVR.WepApi.Hubs;
+
+public class UserPositionHub : Hub<IUserPositionHubClient>
+{
+    public async Task SendPositionsAsync(DevicePosition dto)
+    {
+        var callerConnectionId = Context.ConnectionId;
+        var otherClients = Clients.AllExcept(callerConnectionId);
+        await otherClients.SendPositionsAsync(dto);
+       
+        await Clients.All.SendPositionsAsync(dto);
+    }
+}
